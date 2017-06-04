4f5a20877036d2df88c0cc952ee204054333f2f5
ccli0709 <ccli0709@gmail.com>
2017-06-05 00:20:59 +0800

    Add 最新消息改用firebase來源

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,20 +4,29 @@ import { NavController } from 'ionic-angular';
 import { News } from '../../app/models/news';
 import { NewsService } from '../../app/services/news.service';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage implements OnInit {
 
-  items: News[];
+  // items: News[];
+  items: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController, private newsSerivce: NewsService) { }
-
-  getNews(): void {
-    this.newsSerivce.getNews().then(news => this.items = news);
+  constructor(public navCtrl: NavController, private newsSerivce: NewsService, afDB: AngularFireDatabase) {
+    this.items = afDB.list('/wendance/news', {
+      query: {
+        orderByKey: true
+      }
+    });
   }
+
+  //getNews(): void {
+  //  this.newsSerivce.getNews().then(news => this.items = news);
+  //}
   ngOnInit(): void {
-    this.getNews();
+    //this.getNews();
   }
 }
